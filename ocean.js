91ffdbe72000bfa5fa123c3fee5c11df11a1821a
nahuelvazquez88 <77303438+nahuelvazquez88@users.noreply.github.com>
2022-01-02 00:42:28 -0300

    maquetacion

---
+++ ocean.js
@@ -0,0 +1,6 @@
+let nombre=prompt("ingrese su id por favor  ");
+if (nombre!="nombre"){console.log('hey como estas hoy '+ nombre )}
+else{console.log('error al escribir tu nombre , ingresalo nuevamente ') ;}
+alert('Bienvenido/a Masacre  ' + nombre); 
+
+ 
